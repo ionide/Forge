065ec02d7b8dcb4c3d2fd48b4c6897b99285856b
Andreas Willich <andreas.willich@gmail.com>
2016-01-09 20:26:05 +0100

    add content test for remove file

--- tests/Fix.Tests/Tests.fs
+++ tests/Fix.Tests/Tests.fs
@@ -88,6 +88,30 @@ let ``Remove file from Project - file count``() =
     Assert.AreEqual(2, files)
 
 [<Test>]
+let ``Remove file from Project - content``() =
+    let projectFile = new ProjectFile("foo.fsproj", projectWithFiles)
+    let changedProjectFile = projectFile.RemoveFile "FixProject.fs"
+    let projectContent = changedProjectFile.Content
+
+    let expectedContent = """<?xml version="1.0" encoding="utf-16"?>
+<Project ToolsVersion="12.0" DefaultTargets="Build" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <ItemGroup>
+    <Reference Include="mscorlib" />
+    <Reference Include="FSharp.Core, Version=$(TargetFSharpCoreVersion), Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a">
+      <Private>True</Private>
+    </Reference>
+    <Reference Include="System" />
+    <Reference Include="System.Core" />
+    <Reference Include="System.Numerics" />
+  </ItemGroup>
+  <ItemGroup>
+    <None Include="App.config" />
+    <Compile Include="a_file.fs" />
+  </ItemGroup>
+</Project>"""
+    Assert.AreEqual(expectedContent, projectContent)
+
+[<Test>]
 let ``Remove non existing file from Project - file count``() =
     let projectFile = new ProjectFile("foo.fsproj", projectWithoutFiles)
     let changedProjectFile = projectFile.RemoveFile "FixProject.fs"
