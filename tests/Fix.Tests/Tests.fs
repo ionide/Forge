8fb8c7340bf7c9928bd80e3c535d8281e626fe1a
Andreas Willich <andreas.willich@gmail.com>
2016-01-09 19:38:54 +0100

    add tests for removing files from project

--- tests/Fix.Tests/Tests.fs
+++ tests/Fix.Tests/Tests.fs
@@ -50,3 +50,46 @@ let ``Add file works even if ItemGroup not found`` () =
     let newProject = projectFile.AddFile "file.fs" "Compile"
     let files = newProject.ProjectFiles |> Seq.length
     Assert.AreEqual(1, files)
+
+[<Test>]
+let ``Remove file from Project``() =
+    let project = """<?xml version="1.0" encoding="utf-8"?>
+    <Project ToolsVersion="12.0" DefaultTargets="Build" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+      <ItemGroup>
+        <Reference Include="mscorlib" />
+        <Reference Include="FSharp.Core, Version=$(TargetFSharpCoreVersion), Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a">
+            <Private>True</Private>
+        </Reference>
+        <Reference Include="System" />
+        <Reference Include="System.Core" />
+        <Reference Include="System.Numerics" />
+      </ItemGroup>
+      <ItemGroup>
+        <Compile Include="FixProject.fs" />
+      </ItemGroup>
+    </Project>
+    """
+    let projectFile = new ProjectFile("foo.fsproj", project)
+    let changedProjectFile = projectFile.RemoveFile "FixProject.fs"
+    let files = changedProjectFile.ProjectFiles |> Seq.length
+    Assert.AreEqual(0, files)
+
+[<Test>]
+let ``Remove non existing file from Project``() =
+    let project = """<?xml version="1.0" encoding="utf-8"?>
+    <Project ToolsVersion="12.0" DefaultTargets="Build" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+      <ItemGroup>
+        <Reference Include="mscorlib" />
+        <Reference Include="FSharp.Core, Version=$(TargetFSharpCoreVersion), Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a">
+            <Private>True</Private>
+        </Reference>
+        <Reference Include="System" />
+        <Reference Include="System.Core" />
+        <Reference Include="System.Numerics" />
+      </ItemGroup>
+    </Project>
+    """
+    let projectFile = new ProjectFile("foo.fsproj", project)
+    let changedProjectFile = projectFile.RemoveFile "FixProject.fs"
+    let files = changedProjectFile.ProjectFiles |> Seq.length
+    Assert.AreEqual(0, files)
