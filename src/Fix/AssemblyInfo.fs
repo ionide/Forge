74fc6e8c8b84e9e1fc28ab45ecd940e5ed56b387
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-01-22 23:25:22 +0100

    Bump version to 0.3.2

--- src/Fix/AssemblyInfo.fs
+++ src/Fix/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Fix")>]
 [<assembly: AssemblyProductAttribute("Fix")>]
 [<assembly: AssemblyDescriptionAttribute("Fix is a build tool that provides tasks for creating, compiling, and testing F# projects")>]
-[<assembly: AssemblyVersionAttribute("0.3.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.3.1")>]
+[<assembly: AssemblyVersionAttribute("0.3.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.3.1"
+    let [<Literal>] Version = "0.3.2"
