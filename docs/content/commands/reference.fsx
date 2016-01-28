ee2766a6c1f22adfc86aadc62f54bf9ebad0139a
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-01-28 18:23:36 +0100

    Update documentation

---
+++ docs/content/commands/reference.fsx
@@ -0,0 +1,16 @@
+(**
+## fix reference
+
+References manipulation commands
+
+     reference [add <string>] [remove <string>] [list]
+
+### Options
+
+`add <string>`: Adds a reference to the current folder and project.
+
+`remove <string>`: Removes a reference from the current folder and project.
+
+`list`: List all references of the current project.
+
+*)
