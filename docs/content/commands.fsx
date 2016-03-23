4e1dce7b4fc19c324349daeae624a3f93a63588e
Jonathan Wood <jwood@wintellect.com>
2016-03-23 19:21:45 -0400

    Add some documentation

--- docs/content/commands.fsx
+++ docs/content/commands.fsx
@@ -15,6 +15,7 @@
      paket <string>               - Runs Paket with given arguments
      fake <string>                - Runs FAKE with given arguments
      refresh                      - Refreshes the template cache
+     list gac                     - Lists out all assemblies that is in the Global Assembly Cache
      help                         - Displays this help
      exit                         - Exit interactive mode
 
