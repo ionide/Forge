6a292c18171be5e13a342e65cf5eb692b29183c8
Robert FUSZENECKER <fuszenecker@users.noreply.github.com>
2016-09-27 21:33:36 +0200

    Suave template fix (#163)

--- suave/ApplicationName.fs
+++ suave/ApplicationName.fs
@@ -1,5 +1,3 @@
-#r "<%= packagesPath %>/Suave/lib/net40/Suave.dll"
-
 open Suave                 // always open suave
 open Suave.Successful      // for OK-result
 open Suave.Web             // for config
