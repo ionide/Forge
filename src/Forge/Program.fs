30aace45b4515b46f63c8a038098fbb11269e63e
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-11-09 13:44:39 +0100

    Prompt only when folder is not empty or is not sln

--- src/Forge/Program.fs
+++ src/Forge/Program.fs
@@ -26,12 +26,12 @@ let yellow      = ConsoleColor.Yellow
 
 let parser = ArgumentParser.Create<Command>()
 
-let write color (msg:string) =  
+let write color (msg:string) =
     Console.ForegroundColor <- color
     Console.Write msg
     Console.ForegroundColor <- defaultForeground
 
-let writeln color (msg:string) =  
+let writeln color (msg:string) =
     Console.ForegroundColor <- color
     Console.WriteLine msg
     Console.ForegroundColor <- defaultForeground
@@ -45,13 +45,13 @@ let highlight fcol bcol (msg:string) =
     Console.ForegroundColor <- defaultForeground
     Console.BackgroundColor <- defaultBackground
 
-let highlightln fcol bcol (msg:string) =    
+let highlightln fcol bcol (msg:string) =
     Console.ForegroundColor <- fcol
     Console.BackgroundColor <- bcol
     Console.WriteLine msg
     Console.ForegroundColor <- defaultForeground
     Console.BackgroundColor <- defaultBackground
-    
+
 
 let rec consoleLoop () =
     write   green Environment.CurrentDirectory
@@ -65,21 +65,31 @@ let rec consoleLoop () =
     | Continue    -> consoleLoop ()
     | Result.Exit -> 1
 
+let (|ShouldPrompt|_|) (argv : string []) =
+    let dirs = System.IO.Directory.EnumerateDirectories Environment.CurrentDirectory
+    let files = System.IO.Directory.EnumerateFiles Environment.CurrentDirectory
+
+    if argv.Length <> 0 && argv.[argv.Length - 1] = "--no-prompt" then None
+    elif dirs |> Seq.isEmpty && files |> Seq.isEmpty then None
+    elif dirs |> Seq.exists(fun d -> d.Contains ".git" || d.Contains ".paket" || d.Contains "packages") then None
+    elif files |> Seq.exists(fun d -> d.Contains ".sln" || d.Contains ".gitignore" || d.Contains "paket.dependencies") then None
+    else Some ()
+
 [<EntryPoint>]
 let main argv =
-    let k = 
-        if argv.Length <> 0 && argv.[argv.Length - 1] = "--no-prompt" then 
-            "" 
-        else 
+    let k =
+        match argv with
+        | ShouldPrompt _ ->
             writeln yellow "\nForge should be run from solution/repository root. Please ensure you don't run it from folder containing other solutions"
             writeln yellow "\nDo You want to continue? [Y/n]"
             Console.ReadLine ()
+        | _ -> ""
     if k = "Y" || k = "" then
         match argv with
         | [||] ->
             writeln cyan "\nInitializing Forge... use -h or --help to see commands\n"
             consoleLoop ()
-        | _ ->        
+        | _ ->
             match singlePass argv with
             | Continue -> consoleLoop ()
             | Result.Exit -> 1
