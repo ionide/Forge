2bbfc66bdedd34e2b531b9a5b0e59fc7a1613dc9
Gien Verschatse <gien_verschatse@hotmail.com>
2016-12-23 12:04:40 +0100

    added failing test for rename file

---
+++ tests/Forge.IntegrationTests/RenameFileTests.fs
@@ -0,0 +1,21 @@
+﻿[<NUnit.Framework.TestFixture>]
+[<NUnit.Framework.Category "Rename file">]
+module ``Rename File Tests``
+
+open NUnit.Framework
+open Assertions
+open FsUnit
+
+[<Test>]
+let ``Renaming file changes name in fsproj`` () =
+    let dir = "rename_file"
+
+    ["new project -n Sample --dir src -t console"
+     "new file -n src/Sample/Test --project src/Sample/Sample.fsproj --template fs"
+     "rename file -n src/Sample/Test -r src/Sample/Renamed --project src/Sample/Sample.fsproj"
+    ]
+    |> initTest dir
+
+    let project = dir </> "src" </> "Sample" </> "Sample.fsproj" |> loadProject
+
+    project |> hasFile "Renamed.fs"
