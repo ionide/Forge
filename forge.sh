9435b3c9cb2dfe57db41df006f260d1914ab7ab0
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2019-03-20 12:30:12 +0100

    Updates to build process

--- forge.sh
+++ forge.sh
@@ -2,4 +2,4 @@
 # User just ran a symbolic link which points to real Forge.exe
 # $0 is an absolute path of this symboliklink
 # $* stands for all arguments passed to the command
-mono "$(dirname $(readlink $0))/Forge.exe" $*
+dotnet "Bin/Forge.dll" $*
